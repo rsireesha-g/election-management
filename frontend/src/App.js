daaf88c463affb46cc1cde0e043beef1779e20ab
R Sireesha <92039117+rsiree@users.noreply.github.com>
2025-06-04 15:58:47 +0530

    initial comment

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="main-component">
+      <header className="header">Election Management System</header>
+      <div className="listing-component">
+        <div className='list-name'>Voters</div>
+        <div className='list-name'>Candidates</div>
+        <div className='list-name'>Elections</div>
+        <div className='list-name'>Votes</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
